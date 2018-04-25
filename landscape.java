8214b015d6a77eab66920ba0c57971042970e7e2
ALau5000 <36391914+ALau5000@users.noreply.github.com>
2018-04-24 21:42:29 -0400

    Update landscape.java

--- landscape.java
+++ landscape.java
@@ -1,12 +1,18 @@
 PVector[] birds, rain;
+boolean[] keys = new boolean[128];  
+float playerX;
+float playerY;
 
 void setup() {
   size(800, 500);
 
+  playerX = 400;
+  playerY = 400;
+  
   rain = new PVector[100];
   for(int i = 0; i < rain.length; i++) {
     float x = random(0, width);
-    float y = random(0, 425);
+    float y = random(0, 400);
     rain[i] = new PVector(x, y);
   }
   birds = new PVector[10];
@@ -20,6 +26,7 @@ void setup() {
 void draw() {
   background(#7B68EE);
   
+  move();
   // sun
   noStroke();
   fill(#FF4500);
@@ -32,7 +39,8 @@ void draw() {
   // loop through birds  
   for (int i = 0; i < birds.length; i++) {
     // move each bird
-    birds[i].add(10, 0);
+    float xSpeed = random(0, 20);
+    birds[i].add(xSpeed, 0);
 
     // reset if they go too far
     if (birds[i].x - 75 > width) {
@@ -61,17 +69,60 @@ void draw() {
   }
   
   for(int i = 0; i < rain.length; i++) {
+    float ySpeed = random(0, 30);
     // move each raindrop
-    rain[i].add(0, 10);
+    rain[i].add(0, ySpeed);
 
     // reset if they go too far
-    if (rain[i].y - 100 > height) {
-      rain[i].x = -75;
-      rain[i].y = (int) random(0, 0);
+    if (rain[i].y > height-80) {
+      rain[i].x = (int) random(0, width);
+      rain[i].y = 0;
     }
     
     //draw rain
     fill(#FF0000);
-    rect(rain[i].x, rain[i].y, 5, 20);
+    rect(rain[i].x, rain[i].y, 3, 10);
   }
+   
+  // Player 
+  fill(#7FFF00);
+  arc(playerX, playerY, 70, 70, PI, TWO_PI);
+  fill(#20B2AA);
+  ellipse(playerX, playerY, 150, 30);
+  
+  //Player boundaries
+  if (playerX <= 75) {
+    playerX = 75;
+  } else if (playerX > width - 75) {
+    playerX = width - 75;
+  } if (playerY < 35) {
+    playerY = 35; 
+  } if (playerY > height-100) {
+    playerY = height-100;
+  }
+}
+
+ // player movement
+void move() {
+  if (keys['a']) {
+    playerX -= 10;
+  } if (keys['d']) {
+     playerX += 10;
+  } if (keys['w']) {
+    playerY -= 10;
+  } if (keys['s']) {
+    playerY += 10;
+  }
+}
+
+//The buttons you press become true 
+//multiple keys pressed can be true, 
+//able to apply y and x movement at the same time
+void keyPressed() {
+  keys[key] = true;
+}
+
+//Buttons you release become false
+void keyReleased() {
+  keys[key] = false;
 }
